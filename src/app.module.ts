b680f2bce4d5ba8f98dccfc090fd1647a2ff1e80
luisfuentech <luisfuentes.au@gmail.com>
2024-09-27 23:07:09 -0500

    feat: add Movies module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { MoviesModule } from './modules/movies/movies.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, MoviesModule],
   controllers: [AppController],
   providers: [AppService],
 })
