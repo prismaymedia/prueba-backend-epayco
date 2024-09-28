4efe13d5a1db332688fc0629442234ecb6d82aac
luisfuentech <luisfuentes.au@gmail.com>
2024-09-27 23:27:48 -0500

    feat: add Movie mongoose schema

---
+++ src/modules/movies/repository/schemas/movie.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Movie extends Document {
+  @Prop()
+  _id: string;
+
+  @Prop({ required: true })
+  title: string;
+
+  @Prop([String])
+  directors: string[];
+
+  @Prop([String])
+  cast: string[];
+}
+
+export const MovieSchema = SchemaFactory.createForClass(Movie);
