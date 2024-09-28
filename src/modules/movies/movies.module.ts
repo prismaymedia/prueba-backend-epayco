b3aa3a86c7902adbb5064fffbcbd2a080044410d
luisfuentech <luisfuentes.au@gmail.com>
2024-09-27 23:42:21 -0500

    feat: add Movie schema in Movie module

--- src/modules/movies/movies.module.ts
+++ src/modules/movies/movies.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { MoviesRepository } from './repository/movie.repository';
+import { Movie, MovieSchema } from './repository/schemas/movie.schema';
+
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
 
 @Module({
   controllers: [MoviesController],
-  providers: [MoviesService],
+  providers: [MoviesService, MoviesRepository],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Movie.name,
+        schema: MovieSchema,
+      },
+    ]),
+  ],
 })
 export class MoviesModule {}
