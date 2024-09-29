369ce893a1332a5b03c7d596c73d240715d92e5f
Efraín González <efgonzalezca@gmail.com>
2024-09-28 21:02:07 -0500

    add interceptor for error handling in HTTP requests and add global pipe to validate query param

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 import { envs } from './config';
 import { AppModule } from './app.module';
+import { ApiErrorInterceptor } from './interceptors/api-error.interceptor';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.useGlobalInterceptors(new ApiErrorInterceptor());
   await app.listen(envs.port);
   logger.log(`Application running on http://localhost:${envs.port}`);
 }
